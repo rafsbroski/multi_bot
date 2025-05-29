a6916e366151d45c944191b0be3744f4bf462c1a
Rafael Grilo <rafael@email.com>
2025-05-30 00:13:05 +0100

    Corrige exports em especialistas/__init__.py para alinhamento com bot_controller

--- especialistas/__init__.py
+++ especialistas/__init__.py
@@ -1,1 +1,8 @@
-# Isto permite importar os especialistas como pacote
+# especialistas/__init__.py
+
+from .media_movel_bot import avaliar_media_movel as especialista_moving_average
+from .price_action_bot import analisar_sinal    as especialista_price_action
+from .rsi_bot        import avaliar_rsi         as especialista_rsi
+from .candlestick_bot import analisar_candle    as especialista_candlestick
+from .macd_bot       import analisar_macd       as especialista_macd
+
