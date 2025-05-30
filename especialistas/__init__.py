28d2db1cf957ecd1518292fc52d5a41402ace36c
Rafael Grilo <rafael@email.com>
2025-05-30 19:56:29 +0100

    Corrige imports no __init__.py para usar analisar_sinal em todos os especialistas

--- especialistas/__init__.py
+++ especialistas/__init__.py
@@ -3,9 +3,9 @@
 # Cada especialista exporta a função que de fato existe no seu módulo:
 from .candlestick_bot   import analisar_candle      as especialista_candle
 from .macd_bot          import analisar_macd        as especialista_macd
-from .rsi_bot           import avaliar_rsi           as especialista_rsi
-from .media_movel_bot   import avaliar_media_movel  as especialista_moving_average
-from .price_action_bot  import avaliar_price_action as especialista_price_action
+from .rsi_bot           import analisar_rsi           as especialista_rsi
+from .media_movel_bot   import analisar_media_movel  as especialista_moving_average
+from .price_action_bot  import analisar_price_action as especialista_price_action
 
 __all__ = [
     "especialista_candle",
